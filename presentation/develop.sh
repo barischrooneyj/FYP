9322507964a390a9e799a5f872cbf744eeb9e7f6
Jeremy <barischj@tcd.ie>
2017-03-26 02:05:52 +0100

    watch for changes on per file basis, in develop sript

--- presentation/develop.sh
+++ presentation/develop.sh
@@ -2,8 +2,18 @@
 
 set -eux
 
-pan () { for f in slides/*.org; do pandoc "$f" -s -o "${f%.org}.md"; done }
+pan () { pandoc "$1" -s -o "${1%.org}.md"; }
 
-pan
-when-changed slides/*.org pan &
+# For each org slide:
+#  - create markdown
+#  - create markdown on file change
+for f in slides/*.org; do
+    pan "$f" 
+    when-changed "$f" pan "$f" &
+done
+
+# Kill all spawned processes (file watchers) on exit.
+trap "kill -- -$$" SIGINT SIGTERM EXIT
+
+# Start revealJS server.
 npm start
